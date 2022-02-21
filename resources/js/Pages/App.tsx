b0870f14f067464d41650605645a33843f7f1fa0
danielcarney96 <dancarney@live.co.uk>
2022-02-21 20:45:28 +0000

    Add eslint config.

--- resources/js/Pages/App.tsx
+++ resources/js/Pages/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 
-const App = () => {
-  return <p className='text-blue-600'>Hello world!</p>
-}
+const App: React.FC = () => <p className='text-blue-600'>Hello world!</p>
 
 export default App;
