1e77f094c49eb22cc53b45f9db9dc8e544e3ee37
Dan Butterfield <danrbutterfield@gmail.com>
2022-02-24 22:19:01 +0000

    Add component index

---
+++ resources/js/Components/index.ts
@@ -0,0 +1,21 @@
+import ApplicationLogo from './ApplicationLogo'
+import Button from './Button'
+import Checkbox from './Checkbox'
+import Dropdown from './Dropdown'
+import Input from './Input'
+import Label from './Label'
+import NavLink from './NavLink'
+import ResponsiveNavLink from './ResponsiveNavLink'
+import ValidationErrors from './ValidationErrors'
+
+export {
+  ApplicationLogo,
+  Button,
+  Checkbox,
+  Dropdown,
+  Input,
+  Label,
+  NavLink,
+  ResponsiveNavLink,
+  ValidationErrors,
+}
