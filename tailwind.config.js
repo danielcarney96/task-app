e8840c466ee014fad366def5108c5d152cc65be6
Dan Butterfield <danrbutterfield@gmail.com>
2022-02-24 22:02:31 +0000

    Replace Nunito with Lato

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,22 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.js',
-        './resources/**/*.ts',
-        './resources/**/*.tsx',
-    ],
+  content: [
+    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+    './storage/framework/views/*.php',
+    './resources/views/**/*.blade.php',
+    './resources/js/**/*.js',
+    './resources/**/*.ts',
+    './resources/**/*.tsx',
+  ],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
-            },
-        },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Luto', ...defaultTheme.fontFamily.sans],
+      },
     },
+  },
 
-    plugins: [require('@tailwindcss/forms')],
-};
+  plugins: [require('@tailwindcss/forms')],
+}
