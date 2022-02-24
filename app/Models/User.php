c3dfa950af9e6bc0d543e66d4cb07a5ec3d6f5be
danielcarney96 <dancarney@live.co.uk>
2022-02-24 21:01:16 +0000

    Fix unit test and run code quality tools.

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,6 +53,6 @@ class User extends Authenticatable
 
     public function getFullNameAttribute(): string
     {
-        return $this->first_name . ' ' . $this->last_name;
+        return $this->first_name.' '.$this->last_name;
     }
 }
