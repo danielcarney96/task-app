27285b79bb0c84db2d6e3e8f552d32ef4d72b5d1
Dan Butterfield <danrbutterfield@gmail.com>
2022-03-02 17:46:04 +0000

    Add Story model and migration

---
+++ app/Models/Story.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Story extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'project_id',
+        'sprint_id',
+        'epic_id',
+    ];
+}
