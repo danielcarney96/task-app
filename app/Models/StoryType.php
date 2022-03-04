fe23c553fbb590f997369c81439298d4c1c48d4e
danielcarney96 <dancarney@live.co.uk>
2022-03-04 11:29:00 +0000

    Story type relations.

--- app/Models/StoryType.php
+++ app/Models/StoryType.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class StoryType extends Model
 {
@@ -18,4 +19,9 @@ class StoryType extends Model
         'project_id',
         'type',
     ];
+
+    public function project(): BelongsTo
+    {
+        return $this->belongsTo(Project::class);
+    }
 }
