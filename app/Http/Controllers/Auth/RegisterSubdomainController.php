2c274ceb69c1b39cad9c8fa8c95f4d154c1c8673
danielcarney96 <dancarney@live.co.uk>
2022-04-13 20:25:49 +0100

    Use subdomain name in redirect.

--- app/Http/Controllers/Auth/RegisterSubdomainController.php
+++ app/Http/Controllers/Auth/RegisterSubdomainController.php
@@ -6,7 +6,6 @@ use App\Actions\Auth\CreateSubdomainAction;
 use App\DataTransferObjects\Auth\SubdomainData;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\SubdomainRequest;
-use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -23,10 +22,10 @@ class RegisterSubdomainController extends Controller
 
         $subdomain = $action->execute($data);
 
-        return Inertia::location(route('subdomain.home', ['subdomain' => $subdomain]));
+        return Inertia::location(route('subdomain.home', ['subdomain' => $subdomain->name]));
     }
 
-    public function example($subdomain)
+    public function example(string $subdomain)
     {
         dd($subdomain);
     }
